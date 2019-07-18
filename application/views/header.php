25eabf3820c597eddeb40ca08066ded17aa2f723
yongukmoon <yongukmoon@naver.com>
2019-07-19 01:21:27 +0900

    File

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- 
+        ## File 이용방법
+        
+        1.루트 디렉토리에 css,js,img 디렉토리를 생성
+        2.그안의 파일 경로를 링크하면 적용됨
+        3.application 디렉토리에 넣지 말것!
+     -->
+    <link rel="stylesheet" href="/static/css/style.css">
     <title>Document</title>
 </head>
 <body>
